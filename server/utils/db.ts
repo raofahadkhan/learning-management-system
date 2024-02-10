9cd0942165212f11e7457df310e9e09d3c118160
RAO FAHAD <raofahad046@gmail.com>
2024-02-11 00:22:56 +0500

    resolved a bug in db.ts file

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -6,7 +6,7 @@ const dbUrl: string = process.env.DB_URL || "";
 const connectDB = async () => {
   try {
     await mongoose.connect(dbUrl).then((data: any) => {
-      console.log(`Database Connected with ${data.connect.host}`);
+      console.log(`Database Connected with ${data.connection.host}`);
     });
   } catch (error: any) {
     console.log(error.message);
